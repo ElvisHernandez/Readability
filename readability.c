7f0dc39df88a754bd3b1929184920a299859c4f2
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-13 13:15:17 -0400

    created function to count the number of words in a string

--- readability.c
+++ readability.c
@@ -2,13 +2,15 @@
 #include <cs50.h>
 
 int get_letters(string text);
+int get_words(string text);
 
 int main(void) {
 	// string text = get_string("Text: ");
 	string test_string = "Hello!, how ar3e the people of5 the world today?\n";
 	string test_string2 = "Hello! 5How are you today?";
-	get_letters(test_string2);
-
+	int letters = get_letters(test_string2);
+	get_words(test_string);
+	
 	
 
 }
@@ -20,6 +22,18 @@ int get_letters(string text) {
 			//printf("%c", text[i]);
 			counter++;	
 	}
-	printf("Number of letters: %i\n", counter);
+	//printf("Number of letters: %i\n", counter);
+	return counter;
 }
 
+int get_words(string text) {
+	int counter = 0;
+	for (int i = 0; text[i] != '\0'; i++) {
+		if (text[i] == ' ') {
+			counter++;
+		}
+	}
+	counter++; // for the last word in a sentence
+	// problem stated that sentences will not end with a space
+	printf("Number of words: %i\n",counter);
+}
