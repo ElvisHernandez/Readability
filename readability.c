4e0e1472b278580fd7e809ea0c8f2047fc177ba0
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-13 12:39:33 -0400

    can print all characters input by user

--- readability.c
+++ readability.c
@@ -2,5 +2,13 @@
 #include <cs50.h>
 
 int main(void) {
-	printf("Readability\n");
+	// string text = get_string("Text: ");
+	string test_string = "Hello there world of people\n";
+	int counter = 0;
+	for (int i = 0; test_string[i] != '\0'; i++) {
+		//printf("%c",test_string[i]);
+		if (test_string[i] != ' ') {
+			printf("%c", test_string[i]);
+		}
+	}
 }
